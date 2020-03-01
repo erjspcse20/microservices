3f8ec76d4e23463d528bd62a95220dd127fb7cf2
Bahubali I Mahakal <bahubali@Bahubalis-MacBook-Air.local>
2020-03-01 23:57:35 +0530

    adding feign for call microservices and load ballancer using ribbon

--- currency-conversion-service/src/main/java/com/microservice/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservice/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,14 +14,24 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	private CurrencyExchangeServiceProxy ceproxy;
+	
 	@GetMapping("currency-converter/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversionBean converCurrency(@PathVariable String from
+	public CurrencyConversionBean convertCurrency(@PathVariable String from
 			,@PathVariable String to,@PathVariable BigDecimal quantity) {
 		Map<String,String> uriVariable = new HashMap<>();
 		uriVariable.put("from",from);
 		uriVariable.put("to",to);
 		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8920/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,uriVariable);
 		CurrencyConversionBean resp =  responseEntity.getBody();
-		return new CurrencyConversionBean(resp.getId(),from,to,resp.getConversionMultiple(),quantity,quantity.multiply(resp.getConversionMultiple()),0);
+		return new CurrencyConversionBean(resp.getId(),from,to,resp.getConversionMultiple(),quantity,quantity.multiply(resp.getConversionMultiple()),resp.getPort());
+	}
+	
+	@GetMapping("currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from
+			,@PathVariable String to,@PathVariable BigDecimal quantity) {
+		CurrencyConversionBean resp =  ceproxy.retriveExchangeValue(from, to);
+		return new CurrencyConversionBean(resp.getId(),from,to,resp.getConversionMultiple(),quantity,quantity.multiply(resp.getConversionMultiple()),resp.getPort());
 	}
 }
